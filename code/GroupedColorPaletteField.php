660ea00e3a81b45f086d2b31c809017590042bd5
UndefinedOffset <echipman@webbuildersgroup.com>
2016-01-26 11:12:19 -0400

    Added readonly support to the ColorPalletField and GroupedColorPaletteField classes

--- code/GroupedColorPaletteField.php
+++ code/GroupedColorPaletteField.php
@@ -76,4 +76,53 @@ class GroupedColorPaletteField extends DropdownField
     {
         return 'groupedcolorpalette colorpalette';
     }
+
+    /**
+     * Gets a readonly version of the field
+     * @return GroupedColorPaletteField_Readonly
+     */
+    public function performReadonlyTransformation()
+    {
+		// Source and values are DataObject sets.
+		$field = $this->castedCopy('GroupedColorPaletteField_Readonly');
+		$field->setSource($this->getSource());
+		$field->setReadonly(true);
+
+		return $field;
+	}
+}
+
+class GroupedColorPaletteField_Readonly extends LookupField
+{
+    /**
+     * @param array $properties
+     * @return HTMLText
+     */
+    public function Field($properties = array())
+    {
+        Requirements::css(COLORPALETTE_DIR . '/css/ColorPaletteField.css');
+
+        return parent::Field($properties);
+    }
+    
+    /**
+     * Gets the label of the group that the color appears in
+     * @return string
+     */
+    public function getSelectedGroup()
+    {
+        $source = $this->getSource();
+        
+        if ($source) {
+            foreach($source as $groupName => $values) {
+                if (is_array($values)) {
+                    if (array_key_exists($this->value, $values)) {
+                        return $groupName;
+                    }
+                } else {
+                    throw new InvalidArgumentException('To use GroupedColorPaletteField you need to pass in an array of array\'s');
+                }
+            }
+        }
+    }
 }
---
+++ templates/ColorPaletteField_Readonly.ss
@@ -0,0 +1,6 @@
+<ul id="$ID" class="$extraClass">
+    <li>
+        <input name="$Name" type="hidden" value="$InputValue" />
+        <label for="$ID" style="background-color: $AttrValue"></label>
+    </li>
+</ul>
